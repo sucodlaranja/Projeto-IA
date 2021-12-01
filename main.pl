04b16b0eb30e5e27d90f8e1ce51c3ed1b5d148f2
sucodlaranja <joaoandre210@gmail.com>
2021-12-01 11:33:40 +0000

    update para loop

--- main.pl
+++ main.pl
@@ -3,21 +3,23 @@
 
 
 main :-
+    repeat,
     menu,
     read(X),
-    option(X).
+    option(X),
+    fail.
     
 
     
 
-option(1) :- !,estafetasHeader,estafetas(R),printEstafetas(R).
-option(2) :- !,encomendaHeader,encomendas(R),printEncomendas(R).
-option(3) :- !,transporteHeader,transportes(R),printList(R).
-option(4) :- !,menuEncomenda.
-option(5) :- !,menuestatisticas.
-option(6) :- !,menuEntrega.
-option(7) :- !,printTabelaPreco.
-option(8) :- !,menuMudaPreco.
+option(1) :- !,estafetasHeader,estafetas(R),printEstafetas(R),read(_).
+option(2) :- !,encomendaHeader,encomendas(R),printEncomendas(R),read(_).
+option(3) :- !,transporteHeader,transportes(R),printList(R),read(_).
+option(4) :- !,menuEncomenda,read(_).
+option(5) :- !,menuestatisticas,read(_).
+option(6) :- !,menuEntrega,read(_).
+option(7) :- !,printTabelaPreco,read(_).
+option(8) :- !,menuMudaPreco,read(_).
 option(9) :- !,writeln('Thank you for using our program!!!'),fail.
 option(_) :- write('Not an option').
 
